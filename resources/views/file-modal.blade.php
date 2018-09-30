820be43a466657aef8dc380d9caca150d3ddd9c2
Ruby Ann Matias <bugslayers.hack@gmail.com>
2018-09-30 08:36:38 +0800

    reset

--- resources/views/file-modal.blade.php
+++
@@ -1,5 +0,0 @@
-<form action={{ url('/file/delete')}} method="POST" id="deleteFileForm">
-    @csrf
-    <img src="{{ asset("/images/$file->name") }}" alt="{{ $file->name }}" style="width: 100%;">
-    <input name="file_id" type="number" hidden value="{{ $file->id }}">
-</form>
